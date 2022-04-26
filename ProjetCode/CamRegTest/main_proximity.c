5f6053a8bdd3eeb847f99df1b2020221526d25d0
robin91470 <robin.zimmer@epfl.ch>
2022-04-26 15:03:26 +0200

    1er essai proximity

---
+++ ProjetCode/CamRegTest/main_proximity.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <motors.h>
+#include <camera/po8030.h>
+#include <chprintf.h>
+#include <sensors/proximity.h>
+#include <sensors/VL53L0X/VL53L0X.h>
+#include <audio/play_sound_file.h>
+#include <audio/play_melody.h>
+
+#include <pi_regulator.h>
+#include <process_image.h>
+
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+void SendUint8ToComputer(uint8_t* data, uint16_t size) 
+{
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+}
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+
+    //starts the serial communication
+    serial_start();
+    //start the USB communication
+    usb_start();
+    //starts the camera
+    dcmi_start();
+	po8030_start();
+	//inits the motors
+	motors_init();
+	//uint8_t RAF = 2+2;
+	//stars the threads for the pi regulator and the processing of the image
+	proximity_start();
+	calibrate_ir();
+
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    messagebus_topic_t *proximity_topic = messagebus_find_topic_blocking(&bus, "/proximity");
+    proximity_msg_t proximity_values;
+
+    /* Infinite loop. */
+    while (1) {
+    	//waits 1 second
+
+        messagebus_topic_wait(proximity_topic, &proximity_values, sizeof(proximity_values));
+
+        chprintf((BaseSequentialStream *)&SD3, "%IR1=%.2f IR2=%.2f IR3=%.2f IR4=%.2f IR5=%.2f IR6=%.2f IR7=%.2f IR8=%.2f (%x)\r\n\n",
+        		get_prox(0), get_prox(1), get_prox(2), get_prox(3), get_prox(4), get_prox(5), get_prox(6), get_prox(7));
+        chThdSleepMilliseconds(100);
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 Contient les instructions, idées et la to-do list :
 
 TO DO:
+
+
+INFOS: 
+
+IR Distance sensors: (wiki epuck v.1.3, à confirmer pour le notre)
+-Mesure de la lumière ambiante et la "proximité des objets"
+-Jusqu'à 6cm
+- 8 senseurs
+- 1 pin par senseur (voir doss. électronique), PB 0 et 1 et PC 0 à 5
+-wait on messagebus ? Voir si on peut l'implémenter (similaire au TP chibi : attente de nouvelles mesures)
+-(Sur robot) chopper la position des capteurs IR et LEDs
+
